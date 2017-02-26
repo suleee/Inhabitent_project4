b64485c026ef1c3a518c9e3044337d71edadadcc
Jisu <jaejisu@gmail.com>
2017-02-25 20:57:59 -0800

    add advanture image

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -1,86 +1,81 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * @package RED_Starter_Theme
- */
+* The template for displaying all pages.
+*
+* @package RED_Starter_Theme
+*/
 
 get_header(); ?>
 
 <section class="front-page">
-    <div class ="home-hero"></div>
+  <div class ="home-hero"></div>
 
     <div class="shopstufftitle">
       <h2> Shop Stuff</h2>
           <div class="taxonomy_loop">
-              <?php 
-              $taxonomies  = get_terms( array(
-                'taxonomy' => 'product_type',
-                ) );
-                ?> 
+          <?php $taxonomies  = get_terms( array(
+          'taxonomy' => 'product_type',
+          ) );?> 
 
-                <?php
-                foreach ( $taxonomies  as $term ):?>
-                  <div class="category-wrappper">
-                    <img src="<?php echo get_template_directory_uri() ?>/images/product-type-icons/<?php echo $term->slug?>.svg" alt="<?php echo $term->slug?> icon">
-                  <p> <?php echo $term->description ?> </p>
+        <?php foreach ( $taxonomies  as $term ):?>
+        <div class="category-wrappper">
+          <img src="<?php echo get_template_directory_uri() ?>/images/product-type-icons/<?php echo $term->slug?>.svg" alt="<?php echo $term->slug?> icon">
+            <p> <?php echo $term->description ?> </p>
 
-                  <a href="product_type/<?php echo $term->name;?> " class="category-link"> <?php echo $term->name ?> Stuff</a>
-                  </div>
-                <?php endforeach; ?>
-
-          </div>
-
-
-      
-    </div>
-    <div class="inhabitent-journal-posts">
-      <h2> Inhabitent Journal</h2>
-<div class="frontpageposts">
-    <?php
-    // global $post;
-      $args = array( 
-        'post_type' => 'post', 
-        'order' => 'DSC',
-        'posts_per_page' => 3);
-      $product_posts = get_posts( $args ); // returns an array of posts
-    ?>
-    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-      
-      <!--grab from content-sigle.php-->   
-      <div class="journal-wrapper">
-      <div class="thumbnail-wrapper";>
-      <?php the_post_thumbnail( 'large' ); ?>
-      </div>
-      <div class="journal-text-wrapper">
-     
-      <div class="entry-meta">
-        <?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
-      </div><!-- .entry-meta -->
-      <a class="journal-title" href="<?php the_permalink(); ?>"><?php the_title( '<h3 class="entry-title">', '</h3>' ); ?></a>
-      
-      <a class="read-more" href="<?php the_permalink(); ?>"> Read more </a>
-   </div>
-    </div>
-    <?php endforeach; wp_reset_postdata(); ?>
-    </div>
-
-
-    </div>
-    <div class="latest">
-      <h2> Latest Adventures</h2>
-      <div class="image-container">
-          <div class="firstbox"></div>
-            <div class="container2">
-            <div class="secondbox"></div>
-              <div class="container3">
-              <div class="thirdbox"></div>
-              <div class="fourthbox"></div>
-            </div>
-          </div>
+          <a href="product_type/<?php echo $term->name;?> " class="category-link"> <?php echo $term->name ?> Stuff</a>
+        </div>
+          <?php endforeach; ?>
         </div>
     </div>
 
- </section>
+  <div class="inhabitent-journal-posts">
+    <h2> Inhabitent Journal</h2>
+      <div class="frontpageposts">
+        <?php
+        // global $post;
+        $args = array( 
+        'post_type' => 'post', 
+        'order' => 'DSC',
+        'posts_per_page' => 3);
+        $product_posts = get_posts( $args ); // returns an array of posts
+        ?>
+        <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+  <!--grab from content-sigle.php-->   
+  
+        <div class="journal-wrapper">
+        <div class="thumbnail-wrapper";>
+        <?php the_post_thumbnail( 'large' ); ?>
+        </div>
+        <div class="journal-text-wrapper">
+
+        <div class="entry-meta">
+        <?php red_starter_posted_on(); ?> / <?php comments_number(); ?></div><!-- .entry-meta -->
+        <a class="journal-title" href="<?php the_permalink(); ?>"><?php the_title( '<h3 class="entry-title">', '</h3>' ); ?></a>
+
+        <a class="read-more" href="<?php the_permalink(); ?>"> Read more </a>
+        </div>
+        </div>
+        <?php endforeach; wp_reset_postdata(); ?>
+      </div>
+  </div>
+  
+  <div class="latest">
+      <h2> Latest Adventures</h2>
+      <div class="image-container">
+        <div class="firstbox"> 
+          <p>Getting Back to Nature in a Canoe</p>
+          <a href="">read more</a>
+        </div>
+        <div class="container2">
+          <div class="secondbox"></div>
+          <div class="container3">
+          <div class="thirdbox"></div>
+          <div class="fourthbox"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</section>
 
 <?php get_footer(); ?>
